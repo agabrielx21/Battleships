0c1bb59e577f88f4e1ad50bb68ec5031d31907be
eduardpetre <vasile-eduard.petre@s.unibuc.ro>
2024-05-10 21:30:04 +0300

    Update tableScreen.tsx

--- Battleships/src/screens/game/tableScreen.tsx
+++ Battleships/src/screens/game/tableScreen.tsx
@@ -1,9 +1,5 @@
-import React, { useEffect } from 'react';
-import { GameContext, useGameContext } from '../../hooks/gameContext';
-import { useAuth } from '../../hooks/authContext';
-import { useRoute } from '@react-navigation/native';
-import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
-import Table from "../../components/table";
+import React from 'react';
+import { GameContext } from '../../hooks/gameContext';
 
 const TableScreen = () => {
     return (
